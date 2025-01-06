61e495caf86febf941c43fbe6825be3499e7c3bd
calebluiyee <calebluiyee@cmail.carleton.ca>
2025-01-06 14:33:42 -0500

    added the map, it shows

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,9 +1,22 @@
+'use client';
+
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
+import dynamic from 'next/dynamic';
+import 'leaflet/dist/leaflet.css';
 
-export default function Home() {
-  return <>
-    <Header/>
-    <Hero/>
-  </>
+const DynamicMap = dynamic(() => import('@/sections/Map'), {
+  ssr: false,
+});
+
+export default function Home({ params }: { params: { event: string } }) {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <div style={{ height: '675px', width: '1200px' }}>
+        <DynamicMap params={params} />
+      </div>
+    </>
+  );
 }
