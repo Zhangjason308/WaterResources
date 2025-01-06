a0b18ff0a288a28c94591065f645ec24ec2d4029
Zhangjason308 <zhangjason308@gmail.com>
2025-01-05 22:51:42 -0500

    add hero section

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,5 +1,10 @@
 import { Header } from "@/sections/Header";
+import { Hero } from "@/sections/Hero";
+import ArrowIcon from "@/assets/icons/arrow-right.svg";
 
 export default function Home() {
-  return <Header/>;
+  return <>
+    <Header/>
+    <Hero/>
+  </>
 }
