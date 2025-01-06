2468b718a18cd2832095494df9986286000da73d
Zhangjason308 <zhangjason308@gmail.com>
2025-01-06 15:01:45 -0500

    finished hero section

--- ui/src/sections/Header.tsx
+++ ui/src/sections/Header.tsx
@@ -4,16 +4,16 @@ import MenuIcon from "../assets/menu.svg";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 bg-white shadow-md">
-      <div className="py-5">
+    <header className="sticky top-0 backdrop-blur-sm z-20">
+      <div className="py-6">
         <div className="container">
           <div className="flex items-center justify-between">
             <Image src={Logo} alt="Logo" width={40} height={40} />
             <MenuIcon className="h-6 w-6 md:hidden" />
             <nav className="hidden md:flex gap-6 text-black/60 items-center">
               <a href="#">About Us</a>
+              <a href="#">Tutorial</a>
               <a href="#">Map</a>
-              <a href="#">Post</a>
               <a href="#">Contact Us</a>
             </nav>
           </div>
