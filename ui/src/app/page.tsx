df61a73a27ebb80ca7146e024d8b6de63eee6bcd
Zhangjason308 <zhangjason308@gmail.com>
2025-01-05 23:17:11 -0500

    fix build issues

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
-import ArrowIcon from "@/assets/icons/arrow-right.svg";
 
 export default function Home() {
   return <>
