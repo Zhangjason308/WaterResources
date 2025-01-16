0a30a043f0765311559a9a0646d5b68ce1087cab
Marwan Zeid <45441599+Sgt-Roukoz@users.noreply.github.com>
2025-01-15 21:32:18 -0500

    Implemented Map Panning + Zooming Combined map and search sections into one section. (removed unneeded files)

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -5,13 +5,6 @@ import { Hero } from "@/sections/Hero";
 import MapSection from "@/sections/MapSection";
 import 'leaflet/dist/leaflet.css';
 
-
-function Setup() {
-  let stuff = document.getElementsByTagName('MapContainer')
-  console.log(stuff);
-  return null
-}
-
 export default function Home({ params }: { params: { event: string }}) {
 
     return (
